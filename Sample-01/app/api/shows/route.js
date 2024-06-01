06d0bc830f91b749da5f91ffa3dc230047439225
kimerran <mh.neri@gmail.com>
2024-06-01 15:58:56 +0800

    get session

--- Sample-01/app/api/shows/route.js
+++ Sample-01/app/api/shows/route.js
@@ -1,13 +1,18 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { getAccessToken, withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 import { NextResponse } from 'next/server';
 
 export const GET = withApiAuthRequired(async function shows(req) {
   try {
     const res = new NextResponse();
+    console.log('accessToken', await getSession());
+
     const { accessToken } = await getAccessToken(req, res, {
       scopes: ['read:shows']
     });
     const apiPort = process.env.API_PORT || 3001;
+
+
+
     const response = await fetch(`http://localhost:${apiPort}/api/shows`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
@@ -17,6 +22,7 @@ export const GET = withApiAuthRequired(async function shows(req) {
 
     return NextResponse.json(shows, res);
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error: error.message }, { status: error.status || 500 });
   }
 });
